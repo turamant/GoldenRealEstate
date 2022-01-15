445ce49766aae117b0bd2b47ade82c90e6ac05cb
askvart <tur1amant@gmail.com>
2022-01-15 22:27:40 +0300

    first day coding

---
+++ realtors/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+from datetime import datetime
+
+
+class Realtor(models.Model):
+    name = models.OneToOneField(User, related_name='realtors', on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+
+    def __str__(self):
+        return self.email
